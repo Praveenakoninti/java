a8f8f543b6f27d1b95edbe1e626958756697eada
koninti Praveena <132657121+Praveenakoninti@users.noreply.github.com>
2023-06-27 14:49:13 +0530

    javamock

---
+++ movezeros ex1.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int left = 0;  // pointer to track the non-zero elements
+
+        // Move all non-zero elements to the left side of the array
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                nums[left] = nums[i];
+                left++;
+            }
+        }
+
+        // Fill the remaining elements with zeros
+        while (left < n) {
+            nums[left] = 0;
+            left++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+
+        // Print the modified array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
